2be7d5a3d6c42d80aef935f1980cd1c19e99f8dc
yawei_chiang <yaweichiang@eztravel.com.tw>
2023-03-20 14:54:53 +0800

    add project setting and application

---
+++ src/main/java/chiang/yawei/taiwan_holiday_check/TaiwanHolidayCheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.taiwan_holiday_check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaiwanHolidayCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaiwanHolidayCheckApplication.class, args);
+	}
+
+}
